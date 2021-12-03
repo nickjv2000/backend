b044ff8070ac13c7f34f728defb4dfb236ed1612
Nick <Nickjv2000@hotmail.nl>
2021-12-03 15:00:32 +0100

    Header
    Added a header with buttons & text

---
+++ includes/header.php
@@ -0,0 +1,11 @@
+  <div class="mt-5 align-self-center">
+    <h1> To Do List </h1>
+  </div>
+
+<a class="btn-lg btn btn-dark text-white align-self-center mt-3" href="main.php?">Hoofdscherm</a>
+
+ <a class="btn-lg btn btn-dark text-white align-self-center mt-3" href="createTask.php?">Nieuwe taak </a>
+
+ <a class="btn-lg btn btn-dark text-white align-self-center mt-3" href="updateTask.php">Update taak </a>
+
+ <a class="btn-lg btn btn-dark text-white align-self-center mt-3" href="deleteTask.php">Delete taak </a>
