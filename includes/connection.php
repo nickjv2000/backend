c092f5c6a949b6d6f1904aca9f520bf7f4c0ca17
Nick <Nickjv2000@hotmail.nl>
2021-12-03 13:35:24 +0100

    Start

---
+++ includes/connection.php
@@ -0,0 +1,23 @@
+<?php 
+
+$connec = GetDatabaseConnection();
+
+
+function GetDatabaseConnection() {
+	$servername = "localhost";
+	$username = "root";
+	$password = "mysql";
+	$dbname = "backend";
+
+
+	try {
+ 		$conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+ 		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+ 		return $conn;
+ 		echo "success";
+	}
+	catch(PDOException $e){
+ 		echo "Connection Failed: " . $e->getMessage();
+	}
+}
+?>
