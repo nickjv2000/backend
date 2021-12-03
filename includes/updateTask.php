4685285ed3cf0be4afa706f5c2e53c8fe95950e7
Nick <Nickjv2000@hotmail.nl>
2021-12-03 15:00:59 +0100

    updateTask
    added HTML for redirection

--- includes/updateTask.php
+++ includes/updateTask.php
@@ -18,3 +18,24 @@
 	$statement->execute();
 
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+          integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+	<title>Updated Taak</title>
+</head>
+<body>
+
+<?php
+  include('../includes/connection.php');
+  GetDatabaseConnection();
+?>
+
+<h1 class="mt-5"><center> Taak updated</center></h1>
+<div class="container">
+<center><a class="btn-lg btn btn-dark text-white align-self-center mt-3" href="../php/main.php?">Hoofdscherm</a></center>
+</body>
+</html>
