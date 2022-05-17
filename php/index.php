ad2711e7e9deb451f85c17d8b9e65f842f07eebc
Nick <Nickjv2000@hotmail.nl>
2022-05-17 13:07:42 +0200

    index.php WIP
    Added 2 button for sorting op tijd & status WIP
    
    Added a code to fetch all the lijst results (works)
    Added code to show lijst results WIP
    
    Minor changes to showing the taak results WIP

--- php/index.php
+++ php/index.php
@@ -20,26 +20,64 @@
 <?php 
   include('../includes/header.php');
 ?>
-<br>
+
+<form name="Task properties" class="mt-3" method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+ <button type="submit" name="sort_task_tijd" class="button btn-lg btn btn-light" value="1"> Sorteer op tijd </button>
+ <button type="submit" name="sort_task_status" class="button btn-lg btn btn-light" value="1"> Sorteer op status </button>
+</form>
+
+<?php 
+
+if($_POST['sort_task_tijd'] == true) {
+  "SELECT * FROM `tasks` ORDER BY `tasks`.`tijd` ASC";
+}
+
+if($_POST['sort_task_status'] == true) {
+  "SELECT * FROM tasks ORDER BY `tasks`.`status` ASC";
+}
+?>
+
+<?php 
+ $query = $connec->prepare("SELECT * FROM lijsten");
+ $query->execute();
+ $taking = $query->fetchall();
+ foreach($taking as $counting){
+?>
+<div class="mt-5 float-left mr-3">
+  <div class="card">
+    <div class="card-body">
+      <h1> <?php echo $counting["naam_lijst"]; ?> </h1> 
+    </div>
+  </div>
+</div>
+<?php } ?>
+
+<div class="mt-5">
+
 <?php
   $query = $connec->prepare("SELECT * FROM tasks");
   $query->execute();
   $taken = $query->fetchall();
   foreach($taken as $count){
 ?>
-  <div class="mt-5 float-left mr-3 ">
+  <div class="float-left mr-3">
     <div class="card">
-        <div class="card-body">
-          <h1><u> Taak </u></h1>
-          <h4> <?php echo $count["taak"]; ?> </h4>
-          <h4> <?php echo $count["naam"]; ?> </h4>
-          <h4> <?php echo $count["deadline"]; ?> </h4>
-          <a class="btn-lg btn btn-dark text-white align-self-center mt-3" href="updateTask.php?id=<?= $count["taak_id"] ?>">Update taak </a>
-          <a class="btn-lg btn btn-dark text-white align-self-center mt-3" href="deleteTask.php?id=<?= $count["taak_id"] ?>">Delete taak </a>
-        </div>
+      <div class="card-body">
+        <h1><u> Taak </u></h1>
+        <h4> Taak: <?php echo $count["taak"]; ?> </h4>
+        <h4> Naam: <?php echo $count["naam"]; ?> </h4>
+        <h4> Deadline: <?php echo $count["deadline"]; ?> </h4>
+        <h4> Tijd: <?php echo $count["tijd"]; ?> </h4>
+        <h4> Status: <?php echo $count["status"]; ?> </h4>
+        <a class="btn-lg btn btn-dark text-white align-self-center mt-3" href="updateTask.php?id=<?= $count["taak_id"] ?>">Update taak </a>
+        <a class="btn-lg btn btn-dark text-white align-self-center mt-3" href="deleteTask.php?id=<?= $count["taak_id"] ?>">Delete taak </a>
+      </div>
     </div>
   </div>
-<?php } ?>
 </div>
+</div>
+</div> 
+</div>
+ <?php } ?>
 </body>
 </html>
