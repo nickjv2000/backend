bb14bf8beae342f9ce644c3c1a981b886a636adb
Nick <Nickjv2000@hotmail.nl>
2022-05-17 13:06:45 +0200

    updateTask.php
    Added id, changed variables and a removable part on the bottom. WIP

--- php/updateTask.php
+++ php/updateTask.php
@@ -14,8 +14,10 @@
   $id = $_GET["id"];
   $getid = GetTaskID($id);
   $taak = $getid["taak"];
-  $naam = $getid["naam"];
+  $naam = $getid["naam_persoon"];
   $deadline = $getid["deadline"];
+  $tijd = $getid["tijd"];
+  $status = $getid["status"];
 ?>
 
 <div class="container">
@@ -28,14 +30,28 @@
 
 <div class="align-self-center mt-5">
     <form action="../includes/updateTask.php" method="post">
-        <p class="mt-2"><h4>Taak</h4></p>
+        <p><h4> Id </h4></p>
+        <p><input type="text"  name="taak_id" placeholder="ID" readonly required value="<?php echo $id ?>"></p>
+        <p class="mt-2"><h4> Taak </h4></p>
         <p><input type="text" name="taak" placeholder="Taak" required value="<?php echo $taak ?>"></p>
-        <p><h4>Naam</h4></p>
+        <p><h4> Naam </h4></p>
         <p><input type="text" name="naam" placeholder="Naam" required value="<?php echo $naam ?>"></p>
-        <p><h4>Deadline</h4></p>
+        <p><h4> Deadline </h4></p>
         <p><input type="text" name="deadline" placeholder="Deadline" required value="<?php echo $deadline ?>"></p>
+        <p><h4> Tijd </h4></p>
+        <p><input type="text" name="tijd" placeholder="tijd" required value="<?php echo $tijd ?>"></p>
+        <p><h4> Status </h4></p>
+        <p><input type="text" name="status" placeholder="Status" required value="<?php echo $status ?>">
+        <h5> Opties voor status: Nieuw / Bezig / Klaar
+      </h5></p>
+        <!-- <p><h4><label for="status">Choose a value:</label></h4></p>
+            <select name="status" id="status" readonly>
+                <option value="new">Nieuw</option>
+                <option value="bezig">Bezig</option>
+                <option value="klaar">Afgerond</option>
+            </select> -->
 
-        <p><input class="btn btn-dark" type="submit" onclick="return confirm('Bevestig update')" value="Update"></p>
+        <p><input class="btn btn-dark mt-3" type="submit" onclick="return confirm('Bevestig update')" value="Update"></p>
     </form>
 </div>
 </body>
