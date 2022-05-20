622f2333ee08ac0c176c8b70fe593d25abfad878
Nick <Nickjv2000@hotmail.nl>
2022-05-20 14:27:04 +0200

    header.php
    Removed nieuwe taak

--- includes/header.php
+++ includes/header.php
@@ -5,5 +5,3 @@
 <a class="btn-lg btn btn-dark text-white align-self-center mt-3" href="index.php?">Hoofdscherm</a>
 
 <a class="btn-lg btn btn-dark text-white align-self-center mt-3" href="createList.php?">Nieuwe lijst </a>
-
-<a class="btn-lg btn btn-dark text-white align-self-center mt-3" href="createTask.php?">Nieuwe taak </a>
