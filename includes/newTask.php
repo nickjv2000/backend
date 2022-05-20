1b397203d0e83d6c203ae0c1550859c072ce13bb
Nick <Nickjv2000@hotmail.nl>
2022-05-20 14:28:28 +0200

    newTask.php
    Removed HTML
    Removed connection changed to function
    Removed base code to create a task
    Changed it to a function that calls the code
    Removed all $_POST and made it a function WIP

--- includes/newTask.php
+++ includes/newTask.php
@@ -1,44 +1,7 @@
 <?php 
-	$servername = "localhost";
-	$username = "root";
-	$password = "mysql";
-	$dbname = "backend";
-	$connecter = "mysql:host=" . $servername . ";dbname=" . $dbname . ";";
-
-	$taak = $_POST["taak"];
-	$naam = $_POST["naam"];
-	$deadline = $_POST["deadline"];
-	$tijd = $_POST["tijd"];
-	$status = $_POST["status"];
-
-	$pdo = new PDO($connecter, $username, $password);
-	$statement = $pdo->prepare("INSERT INTO tasks (taak, naam, deadline, tijd, status)
-		VALUES (:taak, :naam, :deadline, :tijd, :status)");
-	$statement->bindParam(":taak", $taak);
-	$statement->bindParam(":naam", $naam);
-	$statement->bindParam(":deadline", $deadline);
-	$statement->bindParam(":tijd", $tijd);
-	$statement->bindParam(":status", $status);
-	$statement->execute();
+	include('../includes/connection.php');
+	GetDatabaseConnection();
+	grabinfo($id, $taak, $naam, $deadline, $tijd, $status);
+	newTask($id, $taak, $naam, $deadline, $tijd, $status, $lijsten_id);
+	header("Location: http://localhost/back-end/backend/php/index.php");
 ?>
-
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
-          integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-	<title>Taak aangemaakt</title>
-</head>
-<body>
-
-<?php
-  include('../includes/connection.php');
-  GetDatabaseConnection();
-?>
-
-<h1 class="mt-5"><center> Taak aangemaakt</center></h1>
-<div class="container">
-<center><a class="btn-lg btn btn-dark text-white align-self-center mt-3" href="../php/index.php?">Hoofdscherm</a></center>
-</body>
-</html>
