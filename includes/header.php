c37607fbffc66253ae23454b2a17ce92dce0ab7d
Nick <Nickjv2000@hotmail.nl>
2022-05-24 13:19:28 +0200

    header.php
    Removed weird piece I had left up for no reason

--- includes/header.php
+++ includes/header.php
@@ -1,7 +1,3 @@
-  <div class="mt-5 align-self-center">
-    <h1> To Do List </h1>
-  </div>
-
 <a class="btn-lg btn btn-dark text-white align-self-center mt-3" href="index.php?">Hoofdscherm</a>
 
 <a class="btn-lg btn btn-dark text-white align-self-center mt-3" href="createList.php?">Nieuwe lijst </a>
