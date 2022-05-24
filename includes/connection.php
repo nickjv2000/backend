e6aeab820c8fa3ee39a88a1d5e155816d2d3807f
Nick <Nickjv2000@hotmail.nl>
2022-05-24 13:02:45 +0200

    connection.php
    Removed grabInfo function
    Added comments
    Added 3 delete functions for
    Tasks where id is equal to taak_id
    Tasks where id is equal to lijsten_id
    Lijsten where id is equal to lijst_id
    Created GetListID to grab Ids from lijsten

--- includes/connection.php
+++ includes/connection.php
@@ -1,9 +1,9 @@
 <?php 
-$connec = GetDatabaseConnection();
-
-/** Maak verbinding met de database door middel van PDO
+/** 
+ * Maak verbinding met de database door middel van PDO
  * Geef een message als de connectie faalt
- * verbind de connectie aan $connec **/
+ * verbind de connectie aan $connec 
+**/
 function GetDatabaseConnection() {
 	$servername = "localhost";
 	$username = "root";
@@ -21,11 +21,29 @@ function GetDatabaseConnection() {
 	}
 }
 
-/** Select alle data van de database 
- * waar het id daat meegegeven word gelijk is aan taak_id in de database **/
+$connec = GetDatabaseConnection();
+
+/** 
+ * Select alle data van de database 
+ * waar het id daat meegegeven word gelijk is
+ * aan taak_id in de database 
+**/
 function GetTaskId($id){
 	$connec = GetDatabaseConnection();
     $query = $connec->prepare("SELECT * FROM tasks WHERE taak_id = :id");
+    $query->bindParam(":id", $id);
+    $query->execute();
+    return $query->fetch();
+}
+
+/** 
+ * Select alle data van de database in lijsten
+ * waar het meegegeven id gelijk staat 
+ * aan de id in de database
+**/
+function GetListId($id){
+	$connec = GetDatabaseConnection();
+    $query = $connec->prepare("SELECT * FROM lijsten WHERE lijst_id = :id");
     $query->bindParam(":id", $id);
     $query->execute();
     return $query->fetch();
@@ -48,28 +66,36 @@ function newList($lijst){
 	$query->execute();
 }
 
-/** Pak alle informatie die word meegegeven
- * Update alle informatie doormiddel van parameters **/
+/** 
+ * Pak alle informatie die word meegegeven
+ * Update alle informatie doormiddel van parameters
+**/
 function updateTask($id, $taak, $naam, $deadline, $tijd, $status){
+	echo $id . " " . $taak . " " . $naam . " " . $deadline . " " . $tijd . " " . $status;
+
 	$connec = GetDatabaseConnection();
-	$query = $connec->prepare("UPDATE tasks SET taak = :taak, naam = :naam, deadline = :deadline, tijd = :tijd, status = :tijd WHERE 'tasks'.'taak_id' =".$id);
+	$query = $connec->prepare("UPDATE tasks SET taak = :taak, naam = :naam, deadline = :deadline, tijd = :tijd, status = :status WHERE taak_id = :id");
 	$query->bindParam(":taak", $taak);
 	$query->bindParam(":naam", $naam);
 	$query->bindParam(":deadline", $deadline);
 	$query->bindParam(":tijd", $tijd);
 	$query->bindParam(":status", $status);
+	$query->bindParam(":id", $id);
 	$query->execute();
 
 }
 
-/** Pak alle infor die word ingevuld 
- * Maak een taak aan bij de ingevoerde lijst door middel van parameters **/
+/** 
+ * Pak alle infor die word ingevuld 
+ * Maak een taak aan bij de ingevoerde lijst 
+ * door middel van parameters 
+**/
 function newTask($id, $taak, $naam, $deadline, $tijd, $status, $lijsten_id){
 	$connec = GetDatabaseConnection();
-	$query = $connec->prepare("INSERT INTO tasks (taak, naam, deadline, tijd, status, lijsten_id)
-		VALUES (:taak, :naam, :deadline, :tijd, :status, :lijsten_id)");
-	$query->bindParam(":taak", $taak);
+	$query = $connec->prepare("INSERT INTO tasks (naam, taak, deadline, tijd, status, lijsten_id)
+		VALUES (:naam, :taak, :deadline, :tijd, :status, :lijsten_id)");
 	$query->bindParam(":naam", $naam);
+	$query->bindParam(":taak", $taak);
 	$query->bindParam(":deadline", $deadline);
 	$query->bindParam(":tijd", $tijd);
 	$query->bindParam(":status", $status);
@@ -77,16 +103,29 @@ function newTask($id, $taak, $naam, $deadline, $tijd, $status, $lijsten_id){
 	$query->execute();
 }
 
-/** Pak het id
- * connect met de database en pak alle informatie die hoort bij het id
- * Om in de code te gebruiken**/
-function grabInfo($id, $taak, $naam, $deadline, $tijd, $status){
-	$id = $_GET["id"];
-  	$getid = GetTaskID($id);
-  	$taak = $getid["taak"];
-  	$naam = $getid["naam"];
-  	$deadline = $getid["deadline"];
-  	$tijd = $getid["tijd"];
-  	$status = $getid["status"];
+/** Delete taak met id gelijk aan taak_id **/
+function deleteTask($id){
+	$connec = GetDatabaseConnection();
+	$query = $connec->prepare("DELETE FROM tasks WHERE taak_id = :id");
+	$query->bindParam(":id", $id);
+	$query->execute();
+}
+
+/** Delete taken met id gelijk aan lijst_id **/
+function deleteTaskList($id){
+	$connec = GetDatabaseConnection();
+	$query = $connec->prepare("DELETE FROM tasks WHERE lijsten_id = :id");
+	$query->bindParam(":id", $id);
+	$query->execute();
+}
+
+/** Delete lijst en alle taken die de lijst_id hebben **/
+function deleteList($id){
+	$connec = GetDatabaseConnection();
+	$query = $connec->prepare("DELETE FROM lijsten WHERE lijst_id = :id");
+	$query->bindParam(":id", $id);
+	$query->execute();
+
+	deleteTaskList($id);
 }
 ?>
